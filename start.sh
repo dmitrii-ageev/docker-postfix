dd04a5a1f28ff2659ccec192936aa79eeee8034a
Dmitrii Ageev <d.ageev@gmail.com>
2017-02-23 00:42:55 +1300

    Changed: file start.sh - now you can specify domain name as an argument to the script.

--- start.sh
+++ start.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
+
+# Domain name can be passes as the first argument to this script
+if [ ! -z "$1" ]; then
+  DOMAINNAME="$1"
+  SERVERNAME="mail.${DOMAINNAME}"
+  MY_DESTINATION="localhost, localhost.localdomain, ${SERVERNAME}, ${DOMAINNAME}"
+  
+  echo ${SERVERNAME} > /etc/hostname
+  echo ${SERVERNAME} > /etc/mailname
+  
+  postconf -e myhostname="${SERVERNAME}"
+  postconf -e mydestination="${MY_DESTINATION}"
+  postconf -e smtpd_banner="${SERVERNAME} ESMTP"
+fi
+ 
 /usr/lib/postfix/master &
 rsyslogd -n
